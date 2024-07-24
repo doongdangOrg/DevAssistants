6ed9ed6ca29a386331f94ea30646a75316e518a3
doongdang <whtjrgus0428@gmail.com>
2024-07-24 23:23:59 +0900

    Init: project

---
+++ src/main/kotlin/com/devassistants/DevAssistantsApplication.kt
@@ -0,0 +1,11 @@
+package com.devassistants
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DevAssistantsApplication
+
+fun main(args: Array<String>) {
+    runApplication<DevAssistantsApplication>(*args)
+}
---
+++ src/test/kotlin/com/devassistants/DevAssistantsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.devassistants
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DevAssistantsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
