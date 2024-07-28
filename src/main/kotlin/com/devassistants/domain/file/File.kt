ad951065a73bf668dc3ae5b314c7aed9f4fa99f9
insoobae <qospwmf@gmailcom>
2024-07-29 02:27:57 +0900

    [Feat] File member 변수 추가

--- src/main/kotlin/com/devassistants/domain/file/File.kt
+++ src/main/kotlin/com/devassistants/domain/file/File.kt
@@ -1,4 +1,20 @@
 package com.devassistants.domain.file
 
-class File {
+
+class File(
+    val memoId: Long? = null,
+
+    // TODO: random uuid및 특수 값 지정
+    val storedFileName: String = "random_uuid",
+
+    val uploadedFileName: String,
+
+    val location: String,
+
+    val extension: String,
+
+    val compressedFormat: String,
+
+    val id: Long? = null,
+) {
 }
