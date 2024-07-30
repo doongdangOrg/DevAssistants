a3797b3da6c3b4157394715cd467cc6beba80618
insoobae <qospwmf@gmailcom>
2024-07-31 02:30:20 +0900

    [Build] actuator, dev-tools, restdocs 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,10 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+    "developmentOnly"("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
